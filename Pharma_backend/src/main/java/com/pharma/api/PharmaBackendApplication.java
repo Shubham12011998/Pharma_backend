5780b80ffdaa8fcdf28f6b015392af2f6a9207ce
Shubham Singh <subhkumar60@gmail.com>
2023-05-08 12:42:36 +0530

    first commit

---
+++ Pharma_backend/src/main/java/com/pharma/api/PharmaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pharma.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmaBackendApplication.class, args);
+	}
+
+}
---
+++ Pharma_backend/src/test/java/com/pharma/api/PharmaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharma.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
